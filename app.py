b6e9baf2b8c297088c31103ea412d70970d5edcc
Adam Syed <adam.syed@rogers.com>
2020-03-17 19:20:01 -0400

    Changed variable name from app to application

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import os
 # Initiate variables
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Initiate app
-app = Flask(__name__)
+# Initiate application
+application = Flask(__name__)
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:msci436project@uni-dss.cmuqyhsxn149.us-east-2.rds.amazonaws.com:3306/uni_dss'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # To prevent any complaints in console
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:msci436project@uni-dss.cmuqyhsxn149.us-east-2.rds.amazonaws.com:3306/uni_dss'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # To prevent any complaints in console
 
 # Initialize db
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 # Initiate marshmallow
-ma = Marshmallow(app)
+ma = Marshmallow(application)
 
 # Student Model/Class
 class Student(db.Model):
@@ -42,7 +42,7 @@ student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
 
 # ENDPOINT - Create a Student
-@app.route('/student-create', methods=['POST'])
+@application.route('/student-create', methods=['POST'])
 def add_student():
     name = request.json['name']
     email = request.json['email']
@@ -56,20 +56,20 @@ def add_student():
     return student_schema.jsonify(new_student)
 
 # ENDPOINT - Get all students
-@app.route('/students-all',methods=['GET'])
+@application.route('/students-all',methods=['GET'])
 def get_students():
     all_students = Student.query.all()
     result = students_schema.dump(all_students)
     return jsonify(result)
 
 # ENDPOINT - Get a student by id
-@app.route('/student/<id>',methods=['GET'])
+@application.route('/student/<id>',methods=['GET'])
 def get_student(id):
     student = Student.query.get(id)
     return student_schema.jsonify(student)
 
 # ENDPOINT - Update a Student
-@app.route('/student/<id>', methods=['PUT'])
+@application.route('/student/<id>', methods=['PUT'])
 def update_student(id):
     student = Student.query.get(id)
 
@@ -87,4 +87,4 @@ def update_student(id):
 
 # Run server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
